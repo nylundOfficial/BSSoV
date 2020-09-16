87bc8c2704bd0f79f1c5edbb108fbfdd8049a912
nylundOfficial <nylundth@arcada.fi>
2020-09-16 19:04:17 +0300

    removed some unnecessary files

--- datasets/.ipynb_checkpoints/datasets-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import yfinance as yf
-import pandas as pd
-import datetime as dt
-from sklearn import preprocessing
-
-def get_data():
-    data = yf.download("SPY AAPL FB GOOG", start="2014-01-01", end=dt.date.today())
-    return data
